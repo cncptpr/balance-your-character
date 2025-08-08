e48410c7e0f5b66cadafb7be8cc2f6580ce9e21d
cncptpr <concept1928@gmail.com>
2025-08-08 21:03:39 +0200

    Start and End UI

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 signal deal_damage(damage: int)
+signal rip()
+
 
 const MAX_HEALTH: int = 100
 var health: int = MAX_HEALTH
@@ -13,14 +15,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func start_figth():
+	print(name, ": Start Figth")
+	$RoundTimer.start()
 
 func attack():
-	deal_damage.emit(randf_range(5, 10))
-	print(name, ": Attacking")
+	if health == 0: return
+	
+	var damage = int(randf_range(5, 10))
+	deal_damage.emit(damage)
+	print(name, ": Attacking with damage ", damage)
 
 func take_damage(damage: int):
 	print(name, ": Took damage ", damage)
 	health = move_toward(health, 0, damage)
-	if (health == 0):
+	if health == 0:
+		print(name, ": RIP")
+		rip.emit()
 		$Sprite2D.flip_v = true
 	$Label.text = str(health) + "/" + str(MAX_HEALTH)
--- scene.gd
+++ scene.gd
@@ -4,11 +4,27 @@ extends Node2D
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func start_figth() -> void:
+	print("Scene: Start Figth")
+	$Player1.start_figth()
+	$Player2.start_figth()
+	$GameStart.queue_free()
 
 func tick_round() -> void:
 	print("Attack!!!! ⚔️")
+
+
+func _on_player_1_rip() -> void:
+	player_won("Right has won")
+
+
+func _on_player_2_rip() -> void:
+	player_won("Left has won")
+	
+func player_won(message: String):
+	$GameEnd/ColorRect/Label.text = message
+	$GameEnd.visible = true
