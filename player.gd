d69d39bdf47db75605b6ab5dfc08c130ca6c81bc
cncptpr <concept1928@gmail.com>
2025-08-08 23:08:42 +0200

    Draft of Equipment System

--- player.gd
+++ player.gd
@@ -3,14 +3,12 @@ extends Node2D
 signal deal_damage(damage: int)
 signal rip()
 
-
 const MAX_HEALTH: int = 100
 var health: int = MAX_HEALTH
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,10 +21,10 @@ func start_figth():
 func attack():
 	if health == 0: return
 	
-	var damage = int(randf_range(5, 10))
-	deal_damage.emit(damage)
-	print(name, ": Attacking with damage ", damage)
-
+	for node in $Inventory.get_children():
+		if node is Equipment:
+			node.use()
+	
 func take_damage(damage: int):
 	print(name, ": Took damage ", damage)
 	health = move_toward(health, 0, damage)
