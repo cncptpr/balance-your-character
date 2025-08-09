17fabd9a82c63cd7b0c4878f79c9eef4fdc57b60
Annymea <anna.grahl@gmx.de>
2025-08-09 12:04:54 +0200

    add death animation

--- player.gd
+++ player.gd
@@ -3,43 +3,19 @@ extends Node2D
 signal deal_damage(damage: float)
 signal rip()
 
-<<<<<<< HEAD
 @export var max_health: float = 100
 var health: float = max_health
-||||||| parent of 0b834f8 (setup animation tree)
-=======
+
 @onready var animationTree : AnimationTree = $Appearance/AnimationTree
 @onready var animationStateMachine: AnimationNodeStateMachinePlayback = animationTree["parameters/playback"]
->>>>>>> 0b834f8 (setup animation tree)
 
 @export var base_stats: EquipmentStats
 var attack_stats: EquipmentStats
 
-<<<<<<< HEAD
-||||||| parent of 0b834f8 (setup animation tree)
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-=======
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animationTree.active = true
 	animationStateMachine.travel("Idle")
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
->>>>>>> 0b834f8 (setup animation tree)
 func start_figth():
 	print(name, ": Start Figth")
 	prepare_attack()
@@ -63,25 +39,17 @@ func prepare_attack() -> void:
 			attack_stats.apply_stats(stats)
 	attack_stats.apply_mods()
 	
+	animationStateMachine.travel("Attack") 
 	$WarmupTimer.start(attack_stats.warmup)
 
 func attack():
 	if health == 0: return
-<<<<<<< HEAD
 	print(name, ": Attacking with ", attack_stats.damage, " Damage")
 	
 	deal_damage.emit(attack_stats.damage)
 	
 	$CooldownTimer.start(attack_stats.cooldown)
 	
-||||||| parent of 0b834f8 (setup animation tree)
-	
-=======
-	animationStateMachine.travel("Attack") 
->>>>>>> 0b834f8 (setup animation tree)
-	var damage = int(randf_range(5, 10))
-	deal_damage.emit(damage)
-	print(name, ": Attacking with damage ", damage)
 
 func take_damage(damage: int):
 	var defend_stats = EquipmentStats.new()
@@ -107,5 +75,4 @@ func take_damage(damage: int):
 func died():
 	print(name, ": RIP")
 	rip.emit()
-	$Appearance/Sprite2D.flip_v = true
-	$Label.text = str(health) + "/" + str(MAX_HEALTH)
+	animationStateMachine.travel("Rip")
